0262ef3a242a4913338cf59dac3a82f69b916be4
Yago2611 <116475207+Yago2611@users.noreply.github.com>
2023-01-30 12:25:12 -0300

    Utilizei a biblioteca json

--- src/io/config.py
+++ src/io/config.py
@@ -3,25 +3,7 @@ from typing import Dict
 def load_config(path: str) -> Dict:
     """ le o arquivo json e retorna como um dicionario """
     arquivo = open(path,"r")
-    x = arquivo.readlines()
-    index = 0
-    for y in x:
-        y = y.replace("{","")
-        y = y.replace("}","")
-        y = y.replace(":","")
-        y = y.replace("\n","")
-        y = y.replace("\"","")
-        y = y.strip()
-        if " " in y:
-            y = y.split()
-        x[index] = y
-        print(y)
-        index+=1
-    x.remove("")
-    x.remove("")
-    x.remove(",")
-    x.remove(",")
-    print(x)
-    dict = {x[0]:{x[1][0]:x[1][1],x[2][0]:x[2][1]},x[3]:{x[4][0]:x[4][1],x[5][0]:x[5][1]},x[6]:{x[7][0]:x[7][1]}}
+    x = arquivo.read()
+    dict = json.loads(x)
     arquivo.close()
     return dict
