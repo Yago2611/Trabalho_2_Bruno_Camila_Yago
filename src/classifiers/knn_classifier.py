c5470ad2a67ee16550096209503b036e2c7195d7
Yago2611 <desouzayago2017@gmail.com>
2023-01-31 22:23:08 -0300

    classificador knn

--- src/classifiers/knn_classifier.py
+++ src/classifiers/knn_classifier.py
@@ -7,11 +7,22 @@ from src.datasets.dataset_interface import DatasetInterface
 class KnnClassifier(ClassifierInterface):
     def __init__(self, config: Dict) -> None:
         super().__init__(config)
+        self.dados_treino = []
+        self.dados_teste = []
+        self.distancias = []
 
     def train(self, train_dataset: DatasetInterface) -> None:
-        # salvar as amostras do dataset
+        for x in range(len(train_dataset.lista)):
+           dado = train_dataset.get(x)
+           self.dados_treino.append(dado)
 
     def predict(self, test_dataset: DatasetInterface) -> List[str]:
         """ para cada amostra no dataset, buscar os k vizinhos mais proximos e 
         retornar a classe mais frequente entre eles """
+        for x in range(len(test_dataset.lista)):
+            dado = test_dataset.get(x)
+            self.dados_teste.append(dado)
+        for teste in self.dados_teste:
+            for treino in self.dados_treino:
+                pass
         return []
--- teste.py
+++ teste.py
@@ -1,7 +1,24 @@
-import cv2
-import sys
-import json
-
-args = sys.argv
-img = cv2.imread("data/datasets/img_small/test/000.png",0) 
-print(img)
+texto = input("")
+texto = texto.split()
+for x in range(len(texto)):
+    for y in range(x+1,len(texto)):
+        if texto[x].upper() > texto[y].upper():
+            temp = texto[x]
+            texto[x] = texto[y]
+            texto[y] = temp
+dic = {}
+cont = 0
+for i in texto:
+    if len(dic) == 0:
+        dic = {i.lower():1}
+    else:
+        for x in dic:
+            if x.upper() == i.upper():
+                dic[x]+=1
+            else:
+                cont+=1
+    if cont == len(dic):
+        dic[i.lower()]=1
+    cont = 0 
+for x in dic:
+    print(x,dic[x])
